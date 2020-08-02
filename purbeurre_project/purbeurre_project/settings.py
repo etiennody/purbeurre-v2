04c04206a78fded6e9b107b4d924983f3f009e01
etiennody <etiennody@gmail.com>
2020-08-02 16:56:34 +0200

    modify default setup project

--- purbeurre_project/purbeurre_project/settings.py
+++ purbeurre_project/purbeurre_project/settings.py
@@ -73,10 +73,18 @@ WSGI_APPLICATION = 'purbeurre_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+PURBEURRE_DBNAME = os.environ['PURBEURRE_DBNAME']
+PURBEURRE_DBUSER = os.environ['PURBEURRE_DBUSER']
+PURBEURRE_DBPASSWD = os.environ['PURBEURRE_DBPASSWD']
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': PURBEURRE_DBNAME,
+        'USER': PURBEURRE_DBUSER,
+        'PASSWORD': PURBEURRE_DBPASSWD,
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -103,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
