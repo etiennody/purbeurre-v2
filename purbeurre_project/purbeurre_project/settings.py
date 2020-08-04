d11c50172cde2c27a774184d7e7d9bcdb031ee76
etiennody <etiennody@gmail.com>
2020-08-04 10:21:17 +0200

    build path to homepage templates

--- purbeurre_project/purbeurre_project/settings.py
+++ purbeurre_project/purbeurre_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'purbeurre_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,5 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 INTERNAL_IPS = ['127.0.0.1']
