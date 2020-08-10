6f35ca9c4518a67bbb670a7cb2ff4e9cd1583b6d
etiennody <etiennody@gmail.com>
2020-08-10 19:02:56 +0200

    add global plan tests

--- purbeurre_project/pages/test_pagestest.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-
-class PurbeurrePagesTests(TestCase):
-    def test_homepage(self):
-        """Test to make sure that our homepage returns an HTTP 200 status code
-        """
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
