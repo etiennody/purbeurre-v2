f82ae47cca3aeb90d1d496b6947b0b3e47f9f17a
Jody Etienne <etiennody@gmail.com>
2020-08-19 10:02:13 +0200

    Task13 search substitute database (#7)
    * generate a product app
    
    * create database with models
    
    * add product and category model tests
    
    * generate-update database fields with models
    
    * add dependencies - requests + pylint-django
    
    * get data from off api
    
    * add m2m models tests
    
    * fix errors on comments

---
+++ purbeurre_project/product/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.TextField(null=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.TextField(null=False, unique=True)
+    nutrition_grade = models.CharField(max_length=1)
+    energy_100g = models.IntegerField()
+    energy_unit = models.TextField()
+    carbohydrates_100g = models.FloatField()
+    sugars_100g = models.FloatField()
+    fat_100g = models.FloatField()
+    saturated_fat_100g = models.FloatField()
+    salt_100g = models.FloatField()
+    sodium_100g = models.FloatField()
+    fiber_100g = models.FloatField()
+    proteins_100g = models.FloatField()
+    image_url = models.URLField()
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class CustomerProduct(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, related_name="wanted", on_delete=models.CASCADE
+    )
+    substitute = models.ForeignKey(
+        Product, related_name="healthy", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        unique_together = [["customer", "product", "substitute"]]
