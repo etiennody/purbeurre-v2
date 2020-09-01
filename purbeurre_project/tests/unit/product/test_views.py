ffef7c6ba793460d38d7c916351270fa63d7ac3c
etiennody <etiennody@gmail.com>
2020-09-01 22:54:03 +0200

    add save and favorites test views

--- purbeurre_project/tests/unit/product/test_views.py
+++ purbeurre_project/tests/unit/product/test_views.py
@@ -1,141 +1,153 @@
-from io import StringIO
-from unittest.mock import patch
-
+from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.test import Client, TestCase, TransactionTestCase
 from django.urls import reverse
 
-from product.models import Category, Product
-
-
-def db_init():
-    """ setUp Test """
-
-    category = Category.objects.create(name="Boissons")
-
-    jus = Product.objects.create(
-        id="1",
-        name="Jus",
-        nutrition_grade="a",
-        energy_100g="2",
-        energy_unit="gr",
-        carbohydrates_100g="2",
-        sugars_100g="2",
-        fat_100g="2",
-        saturated_fat_100g="2",
-        salt_100g="0.2",
-        sodium_100g="0.2",
-        fiber_100g="0.2",
-        proteins_100g="0.2",
-        image_url="http://www.test-jus.fr",
-    )
-
-    rooibos = Product.objects.create(
-        id="2",
-        name="Rooibos",
-        nutrition_grade="b",
-        energy_100g="2",
-        energy_unit="gr",
-        carbohydrates_100g="2",
-        sugars_100g="2",
-        fat_100g="2",
-        saturated_fat_100g="2",
-        salt_100g="0.2",
-        sodium_100g="0.2",
-        fiber_100g="0.2",
-        proteins_100g="0.2",
-        image_url="http://www.test-rooibos.fr",
-    )
-
-    limonade = Product.objects.create(
-        id="3",
-        name="Limonade",
-        nutrition_grade="c",
-        energy_100g="2",
-        energy_unit="gr",
-        carbohydrates_100g="2",
-        sugars_100g="2",
-        fat_100g="2",
-        saturated_fat_100g="2",
-        salt_100g="0.2",
-        sodium_100g="0.2",
-        fiber_100g="0.2",
-        proteins_100g="0.2",
-        image_url="http://www.test-limonade.fr",
-    )
+from product.models import Category, CustomerProduct, Product
 
 
 class ProductTest(TestCase):
     """ Test Product App """
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.client = Client()
-        cls.search_url = reverse("search")
-        db_init()
+    def setUp(self):
+        """Initialyze the set up tests"""
+        user = User.objects.create(
+            username="BobRobert",
+            first_name="Bob",
+            last_name="Robert",
+            email="test_bob@test.com",
+            password="fglZfYmr%?,",
+        )
+        category = Category.objects.create(name="Boissons")
+
+        jus = Product.objects.create(
+            id="1",
+            name="Jus",
+            nutrition_grade="a",
+            energy_100g="2",
+            energy_unit="gr",
+            carbohydrates_100g="2",
+            sugars_100g="2",
+            fat_100g="2",
+            saturated_fat_100g="2",
+            salt_100g="0.2",
+            sodium_100g="0.2",
+            fiber_100g="0.2",
+            proteins_100g="0.2",
+            image_url="http://www.test-jus.fr/jus.jpg",
+            url="http://www.test-jus.fr",
+        )
+        rooibos = Product.objects.create(
+            id="2",
+            name="Rooibos",
+            nutrition_grade="b",
+            energy_100g="2",
+            energy_unit="gr",
+            carbohydrates_100g="2",
+            sugars_100g="2",
+            fat_100g="2",
+            saturated_fat_100g="2",
+            salt_100g="0.2",
+            sodium_100g="0.2",
+            fiber_100g="0.2",
+            proteins_100g="0.2",
+            image_url="http://www.test-rooibos.fr/rooibos.jpg",
+            url="http://www.test-rooibos.fr",
+        )
+        limonade = Product.objects.create(
+            id="3",
+            name="Limonade",
+            nutrition_grade="c",
+            energy_100g="2",
+            energy_unit="gr",
+            carbohydrates_100g="2",
+            sugars_100g="2",
+            fat_100g="2",
+            saturated_fat_100g="2",
+            salt_100g="0.2",
+            sodium_100g="0.2",
+            fiber_100g="0.2",
+            proteins_100g="0.2",
+            image_url="http://www.test-limonade.fr/limonade.jpg",
+            url="http://www.test-limonade.fr",
+        )
 
     # product views
     def test_valid_search_results_url_and_template(self):
-        response = self.client.get(self.search_url + "?q=Jus")
+        response = self.client.get(reverse("search"), {"q": "Jus"})
         self.assertTemplateUsed(response, "product/search_results.html")
         self.assertEqual(response.status_code, 200)
 
     def test_valid_search_results_url_404(self):
-        response = self.client.get(self.search_url + "?q=Jus&page=@")
+        response = self.client.get(reverse("search"), {"q": "Jus", "page": "@"})
         self.assertEqual(response.status_code, 404)
 
     def test_valid_search_results_contains_good_query(self):
-        response = self.client.get(self.search_url + "?q=Limonade")
+        response = self.client.get(reverse("search"), {"q": "Limonade"})
         self.assertContains(response, "Limonade")
 
     def test_valid_search_results_product_found(self):
-        response = self.client.get(self.search_url + "?q=Rooibos")
+        response = self.client.get(reverse("search"), {"q": "Rooibos"})
         self.assertEqual(response.context_data["object_list"].count(), 1)
 
     def test_invalid_search_results_product_ko(self):
-        response = self.client.get(self.search_url + "?q=moutarde")
+        response = self.client.get(reverse("search"), {"q": "Moutarde"})
         self.assertEqual(response.context_data["object_list"].count(), 0)
+
+    
 
     # substitute views
     def test_valid_substitute_results_url_and_template(self):
-        response = self.client.get("/substitute/1")
-        self.assertTemplateUsed(response, "product/substitute_results.html")
+        response = self.client.get(reverse("substitute", args=["1"]))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "product/substitute_results.html")
 
     def test_substitute_better_nutriscore_or_equivalent_and_exclude_id(self):
-        response = self.client.get("/substitute/3")
+        response = self.client.get(reverse("substitute", args=["3"]))
         self.assertEqual(response.status_code, 200)
-        if Product.objects.get(id=2) in response.context_data["object_list"]:
+        self.assertEqual(response.context_data["object_list"].count(), 2)
+        if Product.objects.get(id=1) in response.context_data["object_list"]:
             exclude_id = True
         self.assertTrue(exclude_id)
 
     # details views
-    def test_valid_product_details_template_and_url(self):
-        response = self.client.get("/details/1")
-        self.assertTemplateUsed(response, "product/product_details.html")
+    def test_valid_product_detail_view(self):
+        product = Product.objects.get(name="Jus")
+        response = self.client.get(reverse("details", args=[product.id]))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "product/product_details.html")
 
     def test_invalid_product_details_results(self):
-        response = self.client.get("/details/666")
+        response = self.client.get(reverse("details", args=["666"]))
         self.assertTrue(response.status_code, 404)
 
+    # save views
+    def test_valid_save_page_if_not_logged(self):
+        product = Product.objects.get(name="Jus")
+        substitute = Product.objects.get(name="Rooibos")
+        response = self.client.get(reverse("save"), args=(product.id, substitute.id))
+        self.assertRedirects(
+            response, "/login/?next=/save/", status_code=302, target_status_code=200,
+        )
 
-class ImportOffTest(TestCase):
-    def test_command_output(self):
-        out = StringIO()
-        call_command("import_off", stdout=out)
-        self.assertIn("Data successfully downloaded !", out.getvalue())
+    def test_valid_save_view_if_logged(self):
+        self.client.login(username="BobRobert", password="fglZfYmr%?,")
+        customer = User.objects.get(username="BobRobert").id
+        product = Product.objects.get(name="Jus")
+        substitute = Product.objects.get(name="Rooibos")
+        response = self.client.post(
+            reverse("save"), args=(customer, product.id, substitute.id)
+        )
+        self.assertTrue(response.status_code, 200)
 
+    # favorites views
+    def test_valid_favorites_redirect_template_if_not_logged(self):
+        response = self.client.get(reverse("favorites"))
+        self.assertTemplateUsed(response, "product/favorites.html")
 
-class CommandTest(TransactionTestCase):
-    """ Test BaseCommand with mock and patch"""
-
-    @patch("product.management.commands.import_off", new_callable=bool)
-    def test_import_off_errors(self, mock):
-        mock = False
-
-        with self.assertRaises(TypeError):
-            call_command("import_off", nutriscore="o")
-
-        with self.assertRaises(TypeError):
-            call_command("import_off", category=12)
+    def test_favorites_if_logged_and_zero_product(self):
+        self.client.login(username="BobRobert", password="fglZfYmr%?,")
+        response = self.client.get(reverse("favorites"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "product/favorites.html")
+        self.assertEqual(response.context_data["object_list"].count(), 0)
