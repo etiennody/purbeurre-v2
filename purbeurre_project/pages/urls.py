f42c73d15d58eb517030a02eb061cfd6d3514d10
Jody Etienne <etiennody@gmail.com>
2020-09-08 17:17:26 +0200

    Task16 completion with terms of service (#10)
    * create tos views, urls, html
    
    * update url tos link in footer

--- purbeurre_project/pages/urls.py
+++ purbeurre_project/pages/urls.py
@@ -4,5 +4,18 @@ from django.views.generic import TemplateView
 app_name = "pages"
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html", name="home",))
+    path(
+        "",
+        TemplateView.as_view(
+            template_name="pages/home.html",
+            name="home",
+        ),
+    ),
+    path(
+        "",
+        TemplateView.as_view(
+            template_name="pages/tos.html",
+            name="tos",
+        ),
+    ),
 ]
