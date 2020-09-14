c1fb79cd4452150a59a7599356a9362095c44e5c
etiennody <etiennody@gmail.com>
2020-09-14 14:14:58 +0200

    modify api_off tests with mock and fixture

--- purbeurre_project/tests/unit/product/api_off_tests.py
+++ purbeurre_project/tests/unit/product/api_off_tests.py
@@ -1,17 +1,35 @@
-import json
+"""Mocking tests to processing import data from Open Food Facts Api
 
+Raises:
+    Exception: categories from Open Food Facts endpoints is down
+    Exception: products from Open Food Facts endpoints is down
+"""
+# pylint: disable=redefined-outer-name
 import pytest
 import requests
 import responses
 
+from product.management.commands.import_off import Command
+from product.models import Category, Product
+
 
 @pytest.fixture
 def mocked_responses():
+    """Responses as a pytest fixture
+
+    Yields:
+        generator: code block after the yield statement is executed as teardown code
+    """
     with responses.RequestsMock() as rsps:
         yield rsps
 
 
 def test_valid_status_code_api_off_for_categories_is_success(mocked_responses):
+    """Valid if status code for categories endpoint import is success
+
+    Args:
+        mocked_responses (fixture): test function was called with mocked_responses
+    """
     mocked_responses.add(
         responses.GET,
         "https://fr.openfoodfacts.org/categories.json",
@@ -24,6 +42,11 @@ def test_valid_status_code_api_off_for_categories_is_success(mocked_responses):
 
 
 def test_valid_status_code_api_off_for_products_is_success(mocked_responses):
+    """Valid if status code for products endpoint import is success
+
+    Args:
+        mocked_responses (fixture): test function was called with mocked_responses
+    """
     mocked_responses.add(
         responses.GET,
         "https://fr.openfoodfacts.org/cgi/search.pl?",
@@ -35,43 +58,98 @@ def test_valid_status_code_api_off_for_products_is_success(mocked_responses):
     assert resp.status_code == 200
 
 
-@responses.activate
-def test_valid_api_off_json_category():
-    responses.add(
+@pytest.mark.django_db
+def test_valid_one_category_populated(mocked_responses):
+    """Valid if one category can be populated in database
+
+    Args:
+        mocked_responses (fixture): test function was called with mocked_responses
+    """
+    mocked_responses.add(
         responses.GET,
         "https://fr.openfoodfacts.org/categories.json",
-        json={"categorie": "test"},
+        json={
+            "tags": [
+                {"name": "pate-a-tartiner", "products": 5002},
+                {"name": "Fruits", "products": 5001},
+            ]
+        },
+        status=200,
+        content_type="application/json",
     )
     resp = requests.get("https://fr.openfoodfacts.org/categories.json")
-    assert resp.json() == {"categorie": "test"}
+    command = Command()
+    command.get_populate_categories()
+    assert resp.status_code == 200
+    assert Category.objects.filter(name="pate-a-tartiner").exists()
 
 
-@responses.activate
-def test_valid__api_off_json_product():
-    responses.add(
+def test_import_categories_max_products(mocked_responses):
+    """
+    Valid if categories can be import with
+    equal or greater than 5000 products
+
+    Args:
+        mocked_responses (fixture): test function was called with mocked_responses
+    """
+    mocked_responses.add(
+        responses.GET,
+        "https://fr.openfoodfacts.org/categories.json",
+        status=200,
+        json={
+            "tags": [
+                {"name": "pate-a-tartiner", "products": 2000},
+                {"name": "Fruits", "products": 40000},
+            ]
+        },
+    )
+    command = Command()
+    selected = command.get_populate_categories()
+    assert len(selected) == 1
+    assert selected[0]["name"] == "Fruits"
+    assert selected[0]["products"] == 40000
+
+
+@pytest.mark.django_db
+def test_valid_one_product_populated(mocked_responses):
+    """Valid if one product can be populated in database
+
+    Args:
+        mocked_responses (fixture): test function was called with mocked_responses
+    """
+    mocked_responses.add(
+        responses.GET,
+        "https://fr.openfoodfacts.org/categories.json",
+        json={"tags": [{"name": "category_test", "products": 5002}]},
+    )
+    mocked_responses.add(
         responses.GET,
         "https://fr.openfoodfacts.org/cgi/search.pl?",
         json={
-            "product_name": "test",
-            "nutrition_grade_fr": "a",
-            "url": "http://test.fr",
-            "image_front_url": "http://test.fr/test.jpg",
-            "nutriments": {
-                "energy_value": "1",
-                "energy_unit": "gr",
-                "carbohydrates_100g": "2",
-                "sugars_100g": "2",
-                "fat_100g": "2",
-                "saturated-fat_100g": "2",
-                "salt_100g": "2",
-                "sodium_100g": "2",
-                "fiber_100g": "2",
-                "proteins_100g": "2",
-            },
+            "products": [
+                {
+                    "product_name": "test",
+                    "nutrition_grade_fr": "a",
+                    "url": "http://test.fr",
+                    "image_front_url": "http://test.fr/test.jpg",
+                    "nutriments": {
+                        "energy_value": "1",
+                        "energy_unit": "gr",
+                        "carbohydrates_100g": "2",
+                        "sugars_100g": "2",
+                        "fat_100g": "2",
+                        "saturated-fat_100g": "2",
+                        "salt_100g": "2",
+                        "sodium_100g": "2",
+                        "fiber_100g": "2",
+                        "proteins_100g": "2",
+                    },
+                }
+            ]
         },
+        status=200,
     )
-
-    params = {
+    payload = {
         "action": "process",
         "tagtype_0": "categories",
         "tag_contains_0": "contains",
@@ -80,27 +158,48 @@ def test_valid__api_off_json_product():
         "page_size": 500,
         "json": 1,
     }
-    resp = requests.get("https://fr.openfoodfacts.org/cgi/search.pl?", params=params)
-    assert resp.json() == {
-        "product_name": "test",
-        "nutrition_grade_fr": "a",
-        "url": "http://test.fr",
-        "image_front_url": "http://test.fr/test.jpg",
-        "nutriments": {
-            "energy_value": "1",
-            "energy_unit": "gr",
-            "carbohydrates_100g": "2",
-            "sugars_100g": "2",
-            "fat_100g": "2",
-            "saturated-fat_100g": "2",
-            "salt_100g": "2",
-            "sodium_100g": "2",
-            "fiber_100g": "2",
-            "proteins_100g": "2",
-        },
-    }
-    assert len(responses.calls) == 1
-    assert (
-        responses.calls[0].request.url
-        == "https://fr.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=category_test&sort_by=unique_scans_n&page_size=500&json=1"
+    resp_categories = requests.get("https://fr.openfoodfacts.org/categories.json")
+    resp_product = requests.get(
+        "https://fr.openfoodfacts.org/cgi/search.pl?", params=payload
     )
+    command = Command()
+    command.handle()
+    assert resp_categories.status_code == 200
+    assert resp_product.status_code == 200
+    assert Product.objects.filter(name="test").exists()
+
+
+@responses.activate
+def test_import_raises_categories():
+    """
+    Invalid import data from categories Open Food Fats Api endpoint
+    and raise an exception with a message
+
+    Raises:
+        Exception: categories from Open Food Facts endpoints is down
+    """
+    responses.add(
+        responses.GET, "https://fr.openfoodfacts.org/categories.json", status=404
+    )
+    with pytest.raises(Exception):
+        command = Command()
+        command.get_populate_categories()
+        raise Exception("Cannot import categories from Open Food Facts endpoints")
+
+
+@responses.activate
+def test_import_raises_products():
+    """
+    Invalid import data from products Open Food Fats Api endpoint
+    and raise an exception with a message
+
+    Raises:
+        Exception: products from Open Food Facts endpoints is down
+    """
+    responses.add(
+        responses.GET, "https://fr.openfoodfacts.org/cgi/search.pl?", status=404
+    )
+    with pytest.raises(Exception):
+        command = Command()
+        command.get_populate_categories()
+        raise Exception("Cannot import products from Open Food Facts endpoints")
