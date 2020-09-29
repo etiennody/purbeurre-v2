d84482624283c3e27286aad5adf38ed9cbe02287
etiennody <etiennody@gmail.com>
2020-09-29 14:19:33 +0200

    replace src user profile img to static

--- purbeurre_project/urls.py
+++ purbeurre_project/urls.py
@@ -51,4 +51,7 @@ urlpatterns = [
         name="logout",
     ),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
