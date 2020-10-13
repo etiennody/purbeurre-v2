4b55df0bb9840c84ed924dbacb0c76f8d30354a7
Jody Etienne <etiennody@gmail.com>
2020-10-13 16:48:52 +0200

    Staging (#4)
    * add dev packages
    
    * fix DJANGO_SETTINGS_MODULE
    
    * update travis config
    
    * update ci.py
    
    * add skipping class functional test

---
+++ purbeurre_project/settings/ci.py
@@ -0,0 +1,14 @@
+from .base import *
+
+SECRET_KEY = "x5`4/@wh!=ZRu%&fcA{lrSaV"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    }
+}
