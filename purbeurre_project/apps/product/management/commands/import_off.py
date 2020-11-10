4d3c318712de10901cc34b6a4bd859e23d60d06c
Jody Etienne <etiennody@gmail.com>
2020-11-10 18:50:13 +0100

    Hotfix import off (#12)
    * remove deletion product and category tables
    
    * update import_off
    
    * update api_off_tests.py

--- purbeurre_project/apps/product/management/commands/import_off.py
+++ purbeurre_project/apps/product/management/commands/import_off.py
@@ -6,7 +6,7 @@ import json
 
 import requests
 from django.core.management.base import BaseCommand
-
+from django.db import IntegrityError, transaction
 from product.models import Category, Product
 
 
@@ -48,8 +48,9 @@ class Command(BaseCommand):
         ]
         for category in categories_selected:
             try:
-                categ = Category(name=category["name"])
-                categ.save()
+                with transaction.atomic():
+                    categ = Category(name=category["name"])
+                    categ.save()
             except Exception as exx:
                 print("Une des catégories n'a pu être importée, voici l'erreur:", exx)
         return categories_selected
@@ -99,54 +100,95 @@ class Command(BaseCommand):
             return False
         return True
 
-    def create_products(self, products_list):
-        """Method to create products and add additional categories product in database
+    def update_product(self, product, data):
+        """Method to update database with existing products
+
+        Args:
+            product (object): from Product model
+            data (dictionnary): elements making up a product
+        """
+        try:
+            with transaction.atomic():
+                product.name = data.get("product_name")
+                product.nutrition_grade = data.get("nutrition_grade_fr")
+                product.energy_100g = data["nutriments"].get("energy_value")
+                product.energy_unit = data["nutriments"].get("energy_unit")
+                product.carbohydrates_100g = data["nutriments"].get(
+                    "carbohydrates_100g"
+                )
+                product.sugars_100g = data["nutriments"].get("sugars_100g")
+                product.fat_100g = data["nutriments"].get("fat_100g")
+                product.saturated_fat_100g = data["nutriments"].get(
+                    "saturated-fat_100g"
+                )
+                product.salt_100g = data["nutriments"].get("salt_100g")
+                product.sodium_100g = data["nutriments"].get("sodium_100g")
+                product.fiber_100g = data["nutriments"].get("fiber_100g")
+                product.proteins_100g = data["nutriments"].get("proteins_100g")
+                product.url = data.get("url")
+                product.image_url = data.get("image_front_url")
+                product.save()
+        except IntegrityError:
+            product.delete()
+
+    def create_product(self, data):
+        """Method to create products and add categories with the related product
+
+        Args:
+            data (dictionnary): elements making up a product
+        """
+        product = Product.objects.create(
+            name=data.get("product_name"),
+            nutrition_grade=data.get("nutrition_grade_fr"),
+            energy_100g=data["nutriments"].get("energy_value"),
+            energy_unit=data["nutriments"].get("energy_unit"),
+            carbohydrates_100g=data["nutriments"].get("carbohydrates_100g"),
+            sugars_100g=data["nutriments"].get("sugars_100g"),
+            fat_100g=data["nutriments"].get("fat_100g"),
+            saturated_fat_100g=data["nutriments"].get("saturated-fat_100g"),
+            salt_100g=data["nutriments"].get("salt_100g"),
+            sodium_100g=data["nutriments"].get("sodium_100g"),
+            fiber_100g=data["nutriments"].get("fiber_100g"),
+            proteins_100g=data["nutriments"].get("proteins_100g"),
+            url=data.get("url"),
+            image_url=data.get("image_front_url"),
+        )
+        print(".", end="")
+        category_list = data.get("categories")
+        for category_product in category_list.split(","):
+            catego, _ = Category.objects.get_or_create(name=category_product)
+            product.categories.add(catego)
+
+    def populate_products(self, products_list):
+        """Method to populate database with products
 
         Args:
             products_list (list): a list of products that can be used to create new ones
         """
+
         for product_dict in products_list:
             if not self.validate_product_dict(product_dict):
                 print("S", end="")
                 continue
-            try:
-                product = Product.objects.create(
-                    name=product_dict.get("product_name"),
-                    nutrition_grade=product_dict.get("nutrition_grade_fr"),
-                    energy_100g=product_dict["nutriments"].get("energy_value"),
-                    energy_unit=product_dict["nutriments"].get("energy_unit"),
-                    carbohydrates_100g=product_dict["nutriments"].get(
-                        "carbohydrates_100g"
-                    ),
-                    sugars_100g=product_dict["nutriments"].get("sugars_100g"),
-                    fat_100g=product_dict["nutriments"].get("fat_100g"),
-                    saturated_fat_100g=product_dict["nutriments"].get(
-                        "saturated-fat_100g"
-                    ),
-                    salt_100g=product_dict["nutriments"].get("salt_100g"),
-                    sodium_100g=product_dict["nutriments"].get("sodium_100g"),
-                    fiber_100g=product_dict["nutriments"].get("fiber_100g"),
-                    proteins_100g=product_dict["nutriments"].get("proteins_100g"),
-                    url=product_dict.get("url"),
-                    image_url=product_dict.get("image_front_url"),
+            # if product exists on DB
+            if Product.objects.filter(name=product_dict.get("product_name")).exists():
+                # update product
+                self.update_product(
+                    product=Product.objects.get(name=product_dict.get("product_name")),
+                    data=product_dict,
                 )
-                print(".", end="")
-                category_list = product_dict.get("categories")
-                for category_product in category_list.split(","):
-                    catego, _ = Category.objects.get_or_create(name=category_product)
-                    product.categories.add(catego)
-            except Exception as exx:
-                print("Un des produits n'a pu être importé, voici l'erreur :", exx)
+            else:
+                try:
+                    # create product
+                    self.create_product(product_dict)
+                except Exception as exx:
+                    print("Un des produits n'a pu être importé, voici l'erreur :", exx)
 
     def handle(self, *args, **options):
-        Product.objects.all().delete()
-        Category.objects.all().delete()
-        self.stdout.write(
-            self.style.SUCCESS("Cleaning Product and Category tables successfully!")
-        )
+        """Main method to download data from Open Food Facts API"""
         self.stdout.write("Product downloads in progress...")
         # Process for products
         for category in self.get_populate_categories():
             products = self.get_products_for_category(category["name"])
-            self.create_products(products)
+            self.populate_products(products)
         self.stdout.write(self.style.SUCCESS("Data successfully downloaded !"))
--- purbeurre_project/apps/product/tests/api_off_tests.py
+++ purbeurre_project/apps/product/tests/api_off_tests.py
@@ -8,8 +8,7 @@ Raises:
 import pytest
 import requests
 import responses
-
-from product.management.commands.import_off import Command
+from product.management.commands.import_off import Command as command_import
 from product.models import Category, Product
 
 
@@ -59,7 +58,7 @@ def test_valid_status_code_api_off_for_products_is_success(mocked_responses):
 
 
 @pytest.mark.django_db
-def test_valid_one_category_populated(mocked_responses):
+def test_valid_one_category_populated_in_db(mocked_responses):
     """Valid if one category can be populated in database
 
     Args:
@@ -78,7 +77,7 @@ def test_valid_one_category_populated(mocked_responses):
         content_type="application/json",
     )
     resp = requests.get("https://fr.openfoodfacts.org/categories.json")
-    command = Command()
+    command = command_import()
     selected = command.get_populate_categories()
     assert resp.status_code == 200
     assert len(selected) == 2
@@ -108,7 +107,7 @@ def test_import_categories_max_products(mocked_responses):
             ]
         },
     )
-    command = Command()
+    command = command_import()
     selected = command.get_populate_categories()
     assert len(selected) == 1
     assert selected[0]["name"] == "Fruits"
@@ -116,7 +115,7 @@ def test_import_categories_max_products(mocked_responses):
 
 
 @pytest.mark.django_db
-def test_valid_one_product_populated(mocked_responses):
+def test_valid_one_product_was_populated_in_db(mocked_responses):
     """Valid if one product can be populated in database
 
     Args:
@@ -168,7 +167,7 @@ def test_valid_one_product_populated(mocked_responses):
     resp_product = requests.get(
         "https://fr.openfoodfacts.org/cgi/search.pl?", params=payload
     )
-    command = Command()
+    command = command_import()
     command.handle()
     assert resp_categories.status_code == 200
     assert resp_product.status_code == 200
@@ -181,11 +180,10 @@ def test_valid_one_product_populated(mocked_responses):
 
 
 @pytest.mark.django_db
-def test_valid_create_products():
-    """Valid create_products method with two categories for a product
-    """
-    command = Command()
-    command.create_products(
+def test_valid_populated_products():
+    """Valid create_products method with two categories for a product"""
+    command = command_import()
+    command.populate_products(
         [
             {
                 "product_name": "test",
@@ -217,10 +215,9 @@ def test_valid_create_products():
 
 @pytest.mark.django_db
 def test_valid_existing_category():
-    """Valid create_products method if categories exist
-    """
-    command = Command()
-    command.create_products(
+    """Valid create_products method if categories exist"""
+    command = command_import()
+    command.populate_products(
         [
             {
                 "product_name": "test",
@@ -263,7 +260,7 @@ def test_import_raises_categories():
         responses.GET, "https://fr.openfoodfacts.org/categories.json", status=404
     )
     with pytest.raises(Exception):
-        command = Command()
+        command = command_import()
         command.get_populate_categories()
         raise Exception("Cannot import categories from Open Food Facts endpoints")
 
@@ -281,6 +278,305 @@ def test_import_raises_products():
         responses.GET, "https://fr.openfoodfacts.org/cgi/search.pl?", status=404
     )
     with pytest.raises(Exception):
-        command = Command()
+        command = command_import()
         command.get_populate_categories()
         raise Exception("Cannot import products from Open Food Facts endpoints")
+
+
+@pytest.mark.django_db
+def test_valid_update_one_product_nutriscore_a_to_b_populated(mocked_responses):
+    """Valid if a product can be updated with nutriscore a to b in database
+
+    Args:
+        mocked_responses (fixture): test function was called with mocked_responses
+    """
+    product = Product.objects.create(
+        name="ProductA",
+        nutrition_grade="a",
+        energy_100g="2",
+        energy_unit="gr",
+        carbohydrates_100g="2",
+        sugars_100g="2",
+        fat_100g="2",
+        saturated_fat_100g="2",
+        salt_100g="0.2",
+        sodium_100g="0.2",
+        fiber_100g="0.2",
+        proteins_100g="0.2",
+        image_url="http://www.test-product.fr/product.jpg",
+        url="http://www.test-product.fr",
+    )
+    category = Category.objects.create(name="bar")
+    category.product_set.add(product)
+    mocked_responses.add(
+        responses.GET,
+        "https://fr.openfoodfacts.org/categories.json",
+        json={"tags": [{"name": "foo", "products": 5002}]},
+    )
+    mocked_responses.add(
+        responses.GET,
+        "https://fr.openfoodfacts.org/cgi/search.pl?",
+        json={
+            "products": [
+                {
+                    "product_name": "ProductA",
+                    "nutrition_grade_fr": "b",
+                    "url": "http://www.test-product.fr",
+                    "image_front_url": "http://www.test-product.fr/product.jpg",
+                    "categories": "foo",
+                    "nutriments": {
+                        "energy_value": "1",
+                        "energy_unit": "gr",
+                        "carbohydrates_100g": "2",
+                        "sugars_100g": "2",
+                        "fat_100g": "2",
+                        "saturated-fat_100g": "2",
+                        "salt_100g": "0.2",
+                        "sodium_100g": "0.2",
+                        "fiber_100g": "0.2",
+                        "proteins_100g": "0.2",
+                    },
+                }
+            ]
+        },
+        status=200,
+    )
+    payload = {
+        "action": "process",
+        "tagtype_0": "categories",
+        "tag_contains_0": "contains",
+        "tag_0": "foo",
+        "sort_by": "unique_scans_n",
+        "page_size": 500,
+        "json": 1,
+    }
+    resp_categories = requests.get("https://fr.openfoodfacts.org/categories.json")
+    resp_product = requests.get(
+        "https://fr.openfoodfacts.org/cgi/search.pl?", params=payload
+    )
+    command = command_import()
+    command.handle()
+    assert resp_categories.status_code == 200
+    assert resp_product.status_code == 200
+    assert Product.objects.filter(name="ProductA").exists()
+    assert Product.objects.filter(nutrition_grade="b").exists()
+    assert Product.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_valid_noupdate_for_one_product(mocked_responses):
+    """Valid if a product can be updated without changes in database
+
+    Args:
+        mocked_responses (fixture): test function was called with mocked_responses
+    """
+    product = Product.objects.create(
+        name="ProductA",
+        nutrition_grade="a",
+        energy_100g="2",
+        energy_unit="gr",
+        carbohydrates_100g="2",
+        sugars_100g="2",
+        fat_100g="2",
+        saturated_fat_100g="2",
+        salt_100g="0.2",
+        sodium_100g="0.2",
+        fiber_100g="0.2",
+        proteins_100g="0.2",
+        image_url="http://www.test-product.fr/product.jpg",
+        url="http://www.test-product.fr",
+    )
+    category = Category.objects.create(name="bar")
+    category.product_set.add(product)
+    mocked_responses.add(
+        responses.GET,
+        "https://fr.openfoodfacts.org/categories.json",
+        json={"tags": [{"name": "bar", "products": 5002}]},
+    )
+    mocked_responses.add(
+        responses.GET,
+        "https://fr.openfoodfacts.org/cgi/search.pl?",
+        json={
+            "products": [
+                {
+                    "product_name": "ProductA",
+                    "nutrition_grade_fr": "a",
+                    "url": "http://www.test-product.fr",
+                    "image_front_url": "http://www.test-product.fr/product.jpg",
+                    "categories": "bar",
+                    "nutriments": {
+                        "energy_value": "2",
+                        "energy_unit": "gr",
+                        "carbohydrates_100g": "2",
+                        "sugars_100g": "2",
+                        "fat_100g": "2",
+                        "saturated-fat_100g": "2",
+                        "salt_100g": "0.2",
+                        "sodium_100g": "0.2",
+                        "fiber_100g": "0.2",
+                        "proteins_100g": "0.2",
+                    },
+                }
+            ]
+        },
+        status=200,
+    )
+    payload = {
+        "action": "process",
+        "tagtype_0": "categories",
+        "tag_contains_0": "contains",
+        "tag_0": "bar",
+        "sort_by": "unique_scans_n",
+        "page_size": 500,
+        "json": 1,
+    }
+    resp_categories = requests.get("https://fr.openfoodfacts.org/categories.json")
+    resp_product = requests.get(
+        "https://fr.openfoodfacts.org/cgi/search.pl?", params=payload
+    )
+    command = command_import()
+    command.handle()
+    assert resp_categories.status_code == 200
+    assert resp_product.status_code == 200
+    assert Product.objects.filter(name="ProductA").exists()
+    assert Product.objects.filter(nutrition_grade="a").exists()
+    categ_bar = Category.objects.get(name="bar")
+    assert categ_bar in product.categories.all()
+    assert Product.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_valid_updating_and_new_product_in_same_category(mocked_responses):
+    """Valid if a product a can be updated with another product b to be added
+
+    Args:
+        mocked_responses (fixture): test function was called with mocked_responses
+    """
+    product_a = Product.objects.create(
+        name="ProductA",
+        nutrition_grade="a",
+        energy_100g="2",
+        energy_unit="gr",
+        carbohydrates_100g="2",
+        sugars_100g="2",
+        fat_100g="2",
+        saturated_fat_100g="2",
+        salt_100g="0.2",
+        sodium_100g="0.2",
+        fiber_100g="0.2",
+        proteins_100g="0.2",
+        image_url="http://www.test-product-a.fr/product-a.jpg",
+        url="http://www.test-product-a.fr",
+    )
+    category = Category.objects.create(name="foo")
+    category.product_set.add(product_a)
+    mocked_responses.add(
+        responses.GET,
+        "https://fr.openfoodfacts.org/categories.json",
+        json={
+            "tags": [
+                {"name": "foo", "products": 5002},
+            ]
+        },
+    )
+    mocked_responses.add(
+        responses.GET,
+        "https://fr.openfoodfacts.org/cgi/search.pl?",
+        json={
+            "products": [
+                {
+                    "product_name": "ProductA",
+                    "nutrition_grade_fr": "a",
+                    "url": "http://www.test-product-a.fr",
+                    "image_front_url": "http://www.test-product-a.fr/product-a.jpg",
+                    "categories": "foo",
+                    "nutriments": {
+                        "energy_value": "1",
+                        "energy_unit": "gr",
+                        "carbohydrates_100g": "1",
+                        "sugars_100g": "1",
+                        "fat_100g": "1",
+                        "saturated-fat_100g": "1",
+                        "salt_100g": "0.1",
+                        "sodium_100g": "0.1",
+                        "fiber_100g": "0.1",
+                        "proteins_100g": "0.1",
+                    },
+                },
+                {
+                    "product_name": "ProductB",
+                    "nutrition_grade_fr": "b",
+                    "url": "http://www.test-product-b.fr",
+                    "image_front_url": "http://www.test-product-b.fr/product-b.jpg",
+                    "categories": "foo",
+                    "nutriments": {
+                        "energy_value": "3",
+                        "energy_unit": "gr",
+                        "carbohydrates_100g": "2",
+                        "sugars_100g": "2",
+                        "fat_100g": "2",
+                        "saturated-fat_100g": "2",
+                        "salt_100g": "0.2",
+                        "sodium_100g": "0.2",
+                        "fiber_100g": "0.2",
+                        "proteins_100g": "0.2",
+                    },
+                },
+            ]
+        },
+        status=200,
+    )
+    payload = {
+        "action": "process",
+        "tagtype_0": "categories",
+        "tag_contains_0": "contains",
+        "tag_0": "foo",
+        "sort_by": "unique_scans_n",
+        "page_size": 500,
+        "json": 1,
+    }
+    resp_categories = requests.get("https://fr.openfoodfacts.org/categories.json")
+    resp_product = requests.get(
+        "https://fr.openfoodfacts.org/cgi/search.pl?", params=payload
+    )
+    command = command_import()
+    command.handle()
+    assert resp_categories.status_code == 200
+    assert resp_product.status_code == 200
+    # Test ProductA
+    assert Product.objects.filter(name="ProductA").exists()
+    assert Product.objects.filter(nutrition_grade="a").exists()
+    categ_foo = Category.objects.get(name="foo")
+    assert categ_foo in product_a.categories.all()
+    # Test ProductB
+    assert Product.objects.filter(name="ProductB").exists()
+    assert Product.objects.filter(nutrition_grade="b").exists()
+    product_b_selected = Product.objects.get(name="ProductB")
+    assert categ_foo in product_b_selected.categories.all()
+    # Count in database
+    assert Product.objects.count() == 2
+    assert Category.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_valid_one_category_updated(mocked_responses):
+    """Valid if one category can be upadted in database
+
+    Args:
+        mocked_responses (fixture): test function was called with mocked_responses
+    """
+    Category.objects.create(name="pate-a-tartiner")
+    mocked_responses.add(
+        responses.GET,
+        "https://fr.openfoodfacts.org/categories.json",
+        json={"tags": [{"name": "Pâte-à-Tartiner", "products": 5002}]},
+        status=200,
+        content_type="application/json",
+    )
+    resp = requests.get("https://fr.openfoodfacts.org/categories.json")
+    command = command_import()
+    selected = command.get_populate_categories()
+    assert resp.status_code == 200
+    assert len(selected) == 1
+    assert selected[0]["name"] == "Pâte-à-Tartiner"
+    assert selected[0]["products"] == 5002
